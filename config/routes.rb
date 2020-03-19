ea1b1868feb420e54b2d3caa95df12b477c6a5b6
Crystal <yxcwuu@gmail.com>
2020-03-19 12:08:48 -0400

    Add show action to bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 resources :flights,only: [:index] do 
-  resources :bookings, only: [:new, :create]
+  resources :bookings, only: [:new, :create, :show]
 end
 
 root to: "flights#index"
