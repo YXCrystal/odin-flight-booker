fec659a2853f650dac94bad182dd8cf7c4f5722d
Crystal <yxcwuu@gmail.com>
2020-03-19 01:22:09 -0400

    Add passengers to flights

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_015201) do
+ActiveRecord::Schema.define(version: 2020_03_19_032838) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_015201) do
     t.time "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "passengers"
   end
 
 end
