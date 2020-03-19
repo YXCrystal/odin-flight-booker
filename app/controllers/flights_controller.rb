c41552ea94c8019a4aed1b6215a2b82b7d46615d
Crystal <yxcwuu@gmail.com>
2020-03-18 23:08:11 -0400

    Show airport codes and date in index page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,8 @@
 class FlightsController < ApplicationController
     def index
+        @airport_options = Airport.all.map {|a| a.code}
+        @flight_options = Flight.all.map {|f| f.start.strftime("%d %b %Y")}
+        @order_flights = @flight_options.sort.uniq
+        @flight = Flight.new
     end
 end
