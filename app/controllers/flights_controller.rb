735e5dabe707e8eefc54c49a834d7bab76a581ea
Crystal <yxcwuu@gmail.com>
2020-03-19 01:21:37 -0400

    Add form to allow passengers to look up flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,28 @@
 class FlightsController < ApplicationController
     def index
-        @airport_options = Airport.all.map {|a| a.code}
-        @flight_options = Flight.all.map {|f| f.start.strftime("%d %b %Y")}
-        @order_flights = @flight_options.sort.uniq
-        @flight = Flight.new
+       @flight = Flight.new
+
+        airport_options = Airport.all.map {|a| a.code}
+        @unique_airport = airport_options.uniq
+
+        flight_options = Flight.all.map{|f| f.start.strftime("%d %b %Y")}
+        @order_flights = flight_options.sort.uniq
+
+        if params[:flight]
+            @to_airport = Airport.find_by(code: params[:flight][:to_airport_id])
+            @from_airport = Airport.find_by(code: params[:flight][:from_airport_id])
+        
+            if params[:flight][:start] == "Any Date"
+                @flights = Flight.all.where(to_airport_id: @to_airport.id, from_airport_id: @from_airport.id)
+            else 
+                @flights = Flight.all.where(to_airport_id: @to_airport.id, from_airport_id: @from_airport.id, start: params[:flight][:start])
+            end
+        
+        end
+        
+        
+    end
+
+    def create
     end
 end
