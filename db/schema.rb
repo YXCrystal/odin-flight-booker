d095b2ec8f656169bde7cae1f971ce9c4c30da5d
Crystal <yxcwuu@gmail.com>
2020-03-19 12:09:00 -0400

    Add booking_id to passengers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_062959) do
+ActiveRecord::Schema.define(version: 2020_03_19_155813) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_062959) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end
