b40d5956dc02c1587ebe84e280cfe9ba48f3a355
Crystal <yxcwuu@gmail.com>
2020-03-23 14:19:02 -0400

    Allow bookings to accept nested attributes from passenger

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-    has_many :passengers
+    has_many :passengers, inverse_of: :booking
     belongs_to :flight
 
-    accepts_nested_attributes_for :passengers
+    accepts_nested_attributes_for :passengers, :reject_if => :all_blank
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,6 @@
 class Passenger < ApplicationRecord
     has_many :flights
     belongs_to :booking
+    
     has_many :booked_flights, through: :bookings, source: :flight 
 end
