67b71bf46b7d045e80ebefe11b51e1d846a9cb54
Crystal <yxcwuu@gmail.com>
2020-03-23 15:37:13 -0400

    Add name and email validation to passengers

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
     has_many :passengers, inverse_of: :booking
     belongs_to :flight
 
-    accepts_nested_attributes_for :passengers, :reject_if => :all_blank
+    accepts_nested_attributes_for :passengers
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,8 @@ class Passenger < ApplicationRecord
     has_many :flights
     belongs_to :booking
     
+    validates :name, presence: true
+    validates :email, presence: true
+    
     has_many :booked_flights, through: :bookings, source: :flight 
 end
